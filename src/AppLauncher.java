8b58685080fae43021833cf5b0653f7e0fbc6bf9
Isuru sankhajith <isankhajith@gmail.com>
2023-11-02 21:29:30 +0530

    modified file

--- src/AppLauncher.java
+++ src/AppLauncher.java
@@ -2,10 +2,11 @@ import javax.swing.SwingUtilities;
 
 public class AppLauncher {
     public static void main(String[] args) {
+        // Ensure that Swing-related code is executed on the Event Dispatch Thread (EDT).
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
-
+                // Place your GUI initialization and other Swing code here
             }
         });
     }
